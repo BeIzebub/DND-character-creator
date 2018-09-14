d1069e0d6b890d4c0dfcc7d3c5cb76c860eb7270
Belzebub <neitteim@gmail.com>
2018-09-15 00:35:13 +0400

    test commit to branch dev

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         Character character = new Character();
-        System.out.println(character.getModifier(Constants.STR));
+        System.out.println(character.getModifier(Constants.CON));
     }
 }
